ccde03482fa35952a3eeaa81fa3a9c0cffd3399e
NoelOsiro <noel.osiro@azubiafrica.org>
2024-07-26 19:16:29 +0300

    change

---
+++ src/components/Layouts/AuthLayout.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React, { ReactNode } from "react";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {/* <!-- ===== Page Wrapper Start ===== --> */}
+      <div className="flex items-center justify-center min-h-screen">
+        {/* <!-- ===== Content Area Start ===== --> */}
+        <div className="flex flex-col items-center justify-center">
+          {/* <!-- ===== Main Content Start ===== --> */}
+          <main>
+            <div className="m-auto p-4">
+              {children}
+            </div>
+          </main>
+          {/* <!-- ===== Main Content End ===== --> */}
+        </div>
+        {/* <!-- ===== Content Area End ===== --> */}
+      </div>
+      {/* <!-- ===== Page Wrapper End ===== --> */}
+    </>
+  );
+}
